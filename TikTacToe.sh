b721827a05a2d72ccb7306dfc2167d1128bf273e
malivaibhv15 <pranali.lembhe77@gmail.com>
2020-03-02 17:36:37 +0530

    ADDED : Reset the board for fresh start

--- TikTacToe.sh
+++ TikTacToe.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
-echo "Tik tac toe game"
+read -p "Enter the number of rows and column=" size
+function displayBoard()
+{
+for (( column=0;column<size;column++ ))
+do
+	for (( rows=0;rows<size;rows++ ))
+	do
+		displayBoard[$column,$rows]=""
+	done
+done
+}
+displayBoard
