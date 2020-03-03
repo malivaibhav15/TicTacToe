5e1c647118acf8ef57fc8c6d1611a895069e117e
malivaibhv15 <pranali.lembhe77@gmail.com>
2020-03-03 18:03:08 +0530

    ADDED : Cpu play when his turn arrives

--- TikTacToe.sh
+++ TikTacToe.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 size=3
+declare -A board
 function board()
 {
 	for (( rows=0;rows<$size;rows++ ))
@@ -12,17 +13,7 @@ function board()
 }
 function assigningLetter()
 {
-	read -p "Enter the choice for sign you want 1."O" 2."X"=" choice
-	if [[ $choice -eq 1 ]]
-	then
-		player1="O"
-		player2="X"
-	else
-		player1="X"
-		player2="O"
-	fi
-	echo "Player1 assigned="$player1
-	echo "Player2 assigned="$player2
+	echo "Player1 assigned=X"
 }
 function toss()
 {
@@ -31,7 +22,7 @@ function toss()
 	then
 		echo "Player1 won the toss and elected to play first"
 	else
-		echo "Player2 won the toss and elected to play first"
+		echo "cpu won the toss and elected to play first"
 	fi
 }
 function displayBoard()
@@ -48,7 +39,76 @@ function displayBoard()
 	done
 	echo "----------"
 }
+function chooseCell()
+{
+	read -p "enter your choice=" choice
+	case $choice in
+		1)
+			board[0,0]=X
+			;;
+		2)
+			board[0,1]=X
+			;;
+		3)
+			board[0,2]=X
+			;;
+		4)
+			board[1,0]=X
+			;;
+		5)
+			board[1,1]=X
+			;;
+		6)
+			board[1,2]=X
+			;;
+		7)
+			board[2,0]=X
+			;;
+		8)
+			board[2,1]=X
+			;;
+		9)
+			board[2,2]=X
+			;;
+		*)
+   	echo "Wrong choice"
+   esac
+}
+function chooseCpu()
+{
+	if [[ ${board[1,1]} != O && ${board[1,1]} != X ]]
+	then
+   	board[1,1]=O
+	elif [[ ${board[0,2]} != O && ${board[0,2]} != X ]]
+	then
+   	board[0,2]=O
+	elif [[ ${board[2,0]} != O && ${board[2,0]} != X ]]
+	then
+   	board[2,0]=O
+	elif [[ ${board[2,2]} != O && ${board[2,2]} != X ]]
+	then
+   	board[2,2]=O
+	elif [[ ${board[0,0]} != O && ${board[0,0]} != X ]]
+	then
+   	board[0,0]=O
+	elif [[ ${board[0,1]} != O && ${board[0,1]} != X ]]
+	then
+   	board[0,1]=O
+	elif [[ ${board[1,2]} != O && ${board[1,2]} != X ]]
+	then
+   	board[1,2]=O
+	elif [[ ${board[1,0]} != O && ${board[1,0]} != X ]]
+	then
+   	board[1,0]=O
+	elif [[ ${board[2,1]} != O && ${board[2,1]} != X ]]
+	then
+   	board[2,1]=O
+	fi
+}
+
 board
 assigningLetter
 toss
 displayBoard
+chooseCell
+chooseCpu
