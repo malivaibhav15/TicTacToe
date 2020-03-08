8c9a745beb6bcc2c3a27a9b2b940e9980fcb9c7a
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-08 13:15:30 +0530

    ADDED : Check player is winning or not

--- TikTacToe.sh
+++ TikTacToe.sh
@@ -1,250 +1,249 @@
 #!/bin/bash -x
-size=3
-terms=1
+echo "Welcome to Tic-tac-toe game"
 declare -A board
-function board()
+#CONSTANT
+ROWS=3
+COLUMNS=3
+#VARIABLES
+terms=1
+flagSet=0
+flag=0
+#RESETTING THE BOARD
+function resettingTheBoard()
 {
-	for (( rows=0;rows<$size;rows++ ))
+	for (( index=1; index<=$ROWS; index++ ))
 	do
-		for (( columns=0;columns<$size;columns++ ))
+		for (( index2=1; index2<=$COLUMNS; index2++ ))
 		do
-			board[$rows,$columns]=""
+			board[ $ROWS,$COLUMNS ]=" "
 		done
 	done
 }
-function assigningLetter()
+#FUNCTION FOR ASSIGNING THE LETTER
+function assignLetter()
 {
-	echo "Player1 assigned=X"
+	echo "Player assigned letter X"
+	echo "Computer assigned letter O"
 }
-function toss()
+#FUNCTION TO CHECK WHO PLAY FIRST
+function whoPlayFirst()
 {
-	toss=$((RANDOM%2))
-	if [[ $toss -eq 1 ]]
+	play=$(( $RANDOM % 2 ))
+	if [[ $play -eq 0 ]]
 	then
-		n=2
-		echo "Player1 won the toss and elected to play first"
+		condition=2
+		player=user
+		echo "$player won the toss and elected to play first"
 	else
-		n=1
-		echo "cpu won the toss and elected to play first"
+		player=computer
+		condition=1
+		echo "$player won the toss and elected to play first"
 	fi
 }
+#FUNCTION TO DISPLAY BOARD
 function displayBoard()
 {
-	for ((rows=0;rows<$size;rows++ ))
+	for((index=0;index<$ROWS;index++))
 	do
-		echo "----------"
-		for (( columns=0;columns<$size;columns++ ))
+		echo "----------------"
+		for(( index1=0; index1<$COLUMNS; index1++))
 		do
-			echo -n "|" ${board[$rows,$columns]} " "
+			echo -n "|" ${board[$ROWS,$COLUMNS]} "" "|"
 		done
-		printf "|"
 		echo
 	done
-	echo "----------"
+	echo "----------------"
 }
-function chooseCell()
+#FUNCTION FOR DECIDING THE CELL NUMBER
+function choose()
 {
-	read -p "enter your choice=" choice
+	read -p "Enter your choice=" choice
 	case $choice in
-		1)
-			board[0,0]=X
-			;;
-		2)
-			board[0,1]=X
-			;;
-		3)
-			board[0,2]=X
-			;;
-		4)
-			board[1,0]=X
-			;;
-		5)
-			board[1,1]=X
-			;;
-		6)
-			board[1,2]=X
-			;;
-		7)
-			board[2,0]=X
-			;;
-		8)
-			board[2,1]=X
-			;;
-		9)
-			board[2,2]=X
-			;;
-		*)
-   	echo "Wrong choice"
-   esac
+	1)
+		board[0,0]=X;;
+	2)
+		board[0,1]=X;;
+	3)
+		board[0,2]=X;;
+	4)
+		board[1,0]=X;;
+	5)
+		board[1,1]=X;;
+	6)
+		board[1,2]=X;;
+	7)
+		board[2,0]=X;;
+	8)
+		board[2,1]=X;;
+	9)
+		board[2,2]=X;;
+	*)
+		echo "Wrong choice"
+	esac
 }
-function chooseCpu()
+#COMPUTER PLAY LIKE ME
+function choosecpu()
 {
-	if [[ ${board[1,1]} != O && ${board[1,1]} != X ]]
-	then
-		board[1,1]=O
-	elif [[ ${board[0,2]} != O && ${board[0,2]} != X ]]
-	then
-		board[0,2]=O
-	elif [[ ${board[2,0]} != O && ${board[2,0]} != X ]]
-	then
-		board[2,0]=O
-	elif [[ ${board[2,2]} != O && ${board[2,2]} != X ]]
-	then
-		board[2,2]=O
-	elif [[ ${board[0,0]} != O && ${board[0,0]} != X ]]
-	then
-		board[0,0]=O
-	elif [[ ${board[0,1]} != O && ${board[0,1]} != X ]]
-	then
-		board[0,1]=O
-	elif [[ ${board[1,2]} != O && ${board[1,2]} != X ]]
-	then
-		board[1,2]=O
-	elif [[ ${board[1,0]} != O && ${board[1,0]} != X ]]
-	then
-		board[1,0]=O
-	elif [[ ${board[2,1]} != O && ${board[2,1]} != X ]]
-	then
-		board[2,1]=O
-	fi
+	for (( index=0; index<3; index++ ))
+	do
+		for (( index1=0; index1<3; index1++ ))
+		do
+			if [[ ${board[$index,$index1]} != X && ${board[$index,$index2]} != O ]]
+			then
+				board[$index,$index1]=O
+				index=4
+			fi
+		done
+	done
 }
+#CHECKS WHEATHER PLAYER WIN AND THEN PLAY THAT MOVE
 function checkWin()
 {
-	flagc=0
+	flagSet=0
 	for((counter=0;counter<3;counter++))
 	do
 		if [[ ${board[$counter,0]} == O && ${board[$counter,1]} == O && ${board[$counter,2]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[$counter,2]=O
 			break
 		elif [[ ${board[$counter,1]} == O && ${board[$counter,2]} == O && ${board[$counter,0]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[$counter,0]=O
 			break
 		elif [[ ${board[$counter,2]} == O && ${board[$counter,0]} == O && ${board[$counter,1]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[$counter,1]=O
 			break
 		elif [[ ${board[0,$counter]} == O && ${board[1,$counter]} == O && ${board[2,$counter]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,$counter]=O
 			break
 		elif [[ ${board[1,$counter]} == O && ${board[2,$counter]} == O && ${board[0,$counter]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[0,$counter]=O
 			break
-		elif [[ ${board[2,$counter]} == O && ${board[0,$counter]} == O && ${board[1,$counter]} != X ]]
+		elif [[ ${board[2,$counter$]} == O && ${board[0,$counter]} == O && ${board[1,$counter]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[1,$counter]=O
 			break
 		elif [[ ${board[0,0]} == O && ${board[1,1]} == O && ${board[2,2]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,2]=O
 			break
 		elif [[ ${board[0,0]} == O && ${board[2,2]} == O && ${board[1,1]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[1,1]=O
 			break
-		elif [[ ${board[2,2]} == O && ${board[1,1]} == O && ${board[0,0]} != X ]]
+		elif [[ ${board[2,2] } == O && ${board[1,1]} == O && ${board[0,0]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,2]=O
 			break
 		elif [[ ${board[2,0]} == O && ${board[1,1]} == O && ${board[0,2]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[0,2]=O
 			break
 		elif [[ ${board[0,2]} == O && ${board[1,1]} == O && ${board[2,0]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,0]=O
 			break
 		elif [[ ${board[2,0]} == O && ${board[0,2]} == O && ${board[1,1]} != X ]]
 		then
-			flagc=1
+			flagSet=1
 			board[1,1]=O
 			break
-		elif [[ ${board[2,0]} == O && ${board[0,2]} == O && ${board[1,1]} != X ]]
-		then
-			flagc=1
-			board[1,1]=O
-			break
+#CHECKS WHEATHER OPPONENT WINS IF SO THEN PLAYER WILL BLOCK IT
 		elif [[ ${board[$counter,0]} == X && ${board[$counter,1]} == X && ${board[$counter,2]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[$counter,2]=O
 			break
 		elif [[ ${board[$counter,1]} == X && ${board[$counter,2]} == X && ${board[$counter,0]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[$counter,0]=O
 			break
 		elif [[ ${board[$counter,2]} == X && ${board[$counter,0]} == X && ${board[$counter,1]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[$counter,1]=O
 			break
 		elif [[ ${board[0,$counter]} == X && ${board[1,$counter]} == X && ${board[2,$counter]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,$counter]=O
 			break
 		elif [[ ${board[1,$counter]} == X && ${board[2,$counter]} == X && ${board[0,$counter]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[0,$counter]=O
 			break
 		elif [[ ${board[2,$counter]} == X && ${board[0,$counter]} == X && ${board[1,$counter]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[1,$counter]=O
 			break
 		elif [[ ${board[0,0]} == X && ${board[1,1]} == X && ${board[2,2]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,2]=O
 			break
 		elif [[ ${board[0,0]} == X && ${board[2,2]} == X && ${board[1,1]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[1,1]=O
 			break
 		elif [[ ${board[2,2]} == X && ${board[1,1]} == X && ${board[0,0]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,2]=O
 			break
 		elif [[ ${board[2,0]} == X && ${board[1,1]} == X && ${board[0,2]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[0,2]=O
 			break
 		elif [[ ${board[0,2]} == X && ${board[1,1]} == X && ${board[2,0]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[2,0]=O
 			break
 		elif [[ ${board[2,0]} == X && ${board[0,2]} == X && ${board[1,1]} != O ]]
 		then
-			flagc=1
+			flagSet=1
 			board[1,1]=O
 			break
 		fi
 	done
 }
-board
-assigningLetter
-toss
-displayBoard
-chooseCell
-chooseCpu
-checkWin
+#FUCTION FOR CHECKING TURN AND CHECKING WIN
+function term()
+{
+	condition=2
+	while [[ $terms -le 9 ]]
+	do
+		if [[ $(($condition % 2)) -eq 0 ]]
+		then
+			echo "Player is playing"
+			choose
+			displayBoard
+		else
+			echo "Computer is playing his move"
+			choosecpu
+			displayBoard
+		fi
+		((condition++))
+		((terms++))
+	done
+}
+term
